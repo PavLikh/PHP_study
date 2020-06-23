551b573791b0e27b80f0794fbc1384aeb4d28893
PavLikh <59540515+PavLikh@users.noreply.github.com>
2020-06-23 19:50:05 +0300

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,9 @@
+    <div class="clearfix">
+           <?php showMenu ($menu, $class='main-menu bottom', $sort = SORT_DESC) ?>
+    </div>
+
+    <div class="footer">&copy;&nbsp;<nobr>2018</nobr> Project.</div>
+
+</body>
+</html>
+<?php ob_end_flush() ?>
